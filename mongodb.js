a33679301200b01fc13abb46280713528932b9ca
Larry Kagan <larry@superiocity.com>
2020-06-23 18:42:10 -0400

    Insert and query

--- mongodb.js
+++ mongodb.js
@@ -1,17 +1,32 @@
-const mongodb = require('mongodb');
-
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectId } = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'node-tasks';
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (error, client) => {
     if (error) {
         return console.error('Unable to connect to database.');
     }
 
     const db = client.db(databaseName);
-    db.collection('users').insertOne({
-        name: 'Larry',
-        age: '123',
+
+    db.collection('users').findOne({ name: 'Jen' }, (error, user) => {
+        if (error) {
+            return console.error('Could not fetch');
+        }
+
+        console.log(user);
     });
+
+    db.collection('users').findOne({ name: 'Jen', age: 1 }, (error, user) => {
+        if (error) {
+            return console.error('Could not fetch');
+        }
+
+        console.log(user);
+    });
+
+
 });

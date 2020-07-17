db067525e211fb3b22598d8fd7a1127cb12722b2
Larry Kagan <larry@superiocity.com>
2020-07-17 13:57:26 -0400

    Create task via mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,20 +5,31 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true,
 });
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-    },
-    age: {
-        type: Number,
-    },
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//     },
+//     age: {
+//         type: Number,
+//     },
+// });
+//
+// const me = new User({
+//     name: 'Larry',
+//     age: 46,
+// });
+//
+// me.save()
+//     .then(() => console.log(me))
+//     .catch(error => console.log('Error', error));
+
+const Task = mongoose.model('Task', {
+    completed: {type: Boolean},
+    description: {type: String}
 });
 
-const me = new User({
-    name: 'Larry',
-    age: 46,
-});
+const myTask = new Task({completed: false, description: 'Go to store'});
 
-me.save()
-    .then(() => console.log(me))
-    .catch(error => console.log('Error', error));
+myTask.save()
+    .then((myTask) => console.log('Added task', myTask))
+    .catch((e) => console.log(e));

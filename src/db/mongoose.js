921574435dfd9f5027c27f4dc5b41812621d1739
Larry Kagan <larry@superiocity.com>
2020-07-18 09:42:32 -0400

    Add a validator to models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,35 +1,51 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
 });
 
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String,
-//     },
-//     age: {
-//         type: Number,
-//     },
-// });
-//
-// const me = new User({
-//     name: 'Larry',
-//     age: 46,
-// });
-//
-// me.save()
-//     .then(() => console.log(me))
-//     .catch(error => console.log('Error', error));
-
-const Task = mongoose.model('Task', {
-    completed: {type: Boolean},
-    description: {type: String}
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        validate: (value) => {
+            if (value < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: (value) => {
+            if (! validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
+    }
 });
 
-const myTask = new Task({completed: false, description: 'Go to store'});
+const me = new User({
+    name: 'Mike',
+    email: 'mike@',
+});
 
-myTask.save()
-    .then((myTask) => console.log('Added task', myTask))
-    .catch((e) => console.log(e));
+me.save()
+    .then(() => console.log(me))
+    .catch(error => console.log('Error', error));
+
+// const Task = mongoose.model('Task', {
+//     completed: {type: Boolean},
+//     description: {type: String}
+// });
+//
+// const myTask = new Task({completed: false, description: 'Go to store'});
+//
+// myTask.save()
+//     .then((myTask) => console.log('Added task', myTask))
+//     .catch((e) => console.log(e));

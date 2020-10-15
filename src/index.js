5a310574c05fcf9d6c8dbdfe1835eda57cfd2bb7
Larry Kagan <larry@superiocity.com>
2020-10-15 16:03:42 -0400

    Delete a task

--- src/index.js
+++ src/index.js
@@ -97,7 +97,7 @@ app.post('/tasks', async (req, res) => {
 
 app.get('/tasks', async (req, res) => {
     try {
-        const tasks = await Task.find({})
+        res.send(await Task.find({}));
     } catch (e) {
         res.status(500).send()
     }
@@ -139,5 +139,19 @@ app.patch('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch (e) {
         res.status(400).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        return res.send(task)
+    } catch (e) {
+        return res.status(500).send(e)
     }
 })

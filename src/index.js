8ac23c6f67f1a2caa6aa17f71f0dba0cf718fd40
Larry Kagan <larry@superiocity.com>
2020-10-16 18:29:50 -0400

    Refactor user routes into own router file

--- src/index.js
+++ src/index.js
@@ -5,84 +5,13 @@ const Task = require('./models/task')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const userRouter = require('./Routers/user')
+
 app.listen(port, () => console.log('Server is up on port ' + port))
 
 // Automatically parse incoming JSON to an object.
 app.use(express.json())
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch(e) {
-        res.status(500).send(e)
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        return  res.send(user)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
-
-app.patch('/users/:id', async (req, res) => {
-    const allowedUpdates = ['age', 'name', 'email', 'password']
-    const updates = Object.keys(req.body)
-    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
-
-    if (! isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!'})
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true, runValidators: true }
-        )
-
-        if (! user) {
-            return res.status(404).send()
-        }
-
-        res.send(user);
-    }  catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-
-        if (! user) {
-            return res.status(404).send()
-        }
-
-        return res.send(user)
-    } catch (e) {
-        return res.status(500).send()
-    }
-})
+app.use(userRouter)
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)

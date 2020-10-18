a4b92a61c39d7d96cf0b33135564be4fc4439730
Larry Kagan <larry@superiocity.com>
2020-10-18 10:23:33 -0400

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 require('./db/mongoose')
-const mongoose = require('mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const userRouter = require('./Routers/user')
 const taskRouter = require('./Routers/task')
 
-app.listen(port, () => console.log('Server is up on port ' + port))
-
-// Automatically parse incoming JSON to an object.
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
+app.listen(port, () => console.log('Server is up on port ' + port))

e94f8163c04f7e6fef340a44d4385136b0c00bd8
Larry Kagan <larry@superiocity.com>
2020-10-28 13:33:20 -0400

    Play with jwt

--- src/index.js
+++ src/index.js
@@ -10,3 +10,16 @@ app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port, () => console.log('Server is up on port ' + port))
+
+const jwt = require('jsonwebtoken');
+
+const myFunction = async () => {
+    const secret = 'thisismynewcourse'
+    const token = jwt.sign({ _id: 'abc123' }, secret, { expiresIn: '7 days'})
+    console.log(token)
+
+    const data = jwt.verify(token, secret)
+    console.log(data)
+}
+
+myFunction()
